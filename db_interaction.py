eee1e5ef75f2a26d835684956c3adf657de27622
pmlobanov <lobanovpm@yandex.ru>
2025-01-15 18:41:38 +0300

    fix: connection to the database moved to each class method

--- db_interaction.py
+++ db_interaction.py
@@ -29,43 +29,44 @@ class DataBaseConnect:
     """
 
     # Поля сonn - соединение с бд.
-    def __init__(self):
+    def create_connection(self):
         self.conn = sqlite3.connect('purchases.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
         self.cursor = self.conn.cursor()
         print("Connection opened!")
 
-    def __del__(self):
-        self.conn.commit()
-        self.conn.close()
-        print("Connection closed!")
 
     def init_db(self) -> None:
         """
                 Создает таблицы в базе данных, если они не существуют.
         """
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS User(
+        conn = sqlite3.connect('purchases.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        cursor = conn.cursor()
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS User(
         User_id INTEGER   PRIMARY KEY,
         User_tg_name VARCHAR(32) NOT NULL)''')
 
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS User_product(
+        cursor.execute('''CREATE TABLE IF NOT EXISTS User_product(
         User_product_id INTEGER   PRIMARY KEY,
         User_in_id INT,
         Product_in_id INT,
         FOREIGN KEY (User_in_id) REFERENCES User (User_id),
         FOREIGN KEY (Product_in_id) REFERENCES Product (Product_sku))''')
 
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Product(
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Product(
         Product_sku INT PRIMARY KEY NOT NULL)''')
 
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Product_cost(
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Product_cost(
         Product_cost_id INTEGER PRIMARY KEY,
         Product_sku  INT NOT NULL,
         Date timestamp,
         Cost INT NOT NULL,
         FOREIGN KEY (Product_cost_id) REFERENCES Product (Product_sku))''')
 
+        conn.commit()
+        conn.close()
         print("Tables created!")
-        self.conn.commit()
+        print("Connection closed!")
 
     def get_all_product_for_user(self, user_tg_id) -> list:
         """
@@ -74,13 +75,18 @@ class DataBaseConnect:
                 :param user_tg_id: Telegram ID пользователя.
                 :return: Список продуктов.
         """
-        self.cursor.execute('''SELECT Product_sku FROM User 
+        conn = sqlite3.connect('purchases.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        cursor = conn.cursor()
+
+        cursor.execute('''SELECT Product_sku FROM User 
         Inner Join User_product 
             on User.User_id= User_product.User_in_id
         Inner Join Product
             on User_product.Product_in_id = Product.Product_sku
                 where user_tg_name = ?''', (user_tg_id,))
-        result = list(zip(*self.cursor.fetchall()))
+        result = list(zip(*cursor.fetchall()))
+        conn.commit()
+        conn.close()
         return result
 
     def start_tracking_for_user(self, user_tg_id, product_data: parser.PriceInfo):
@@ -91,34 +97,39 @@ class DataBaseConnect:
         :param user_tg_id: Telegram ID пользователя.
         :param product_data: Данные о продукте (объект PriceInfo).
         """
+        conn = sqlite3.connect('purchases.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        cursor = conn.cursor()
+
         # Проверка наличия пользователя
-        self.cursor.execute('SELECT User_id FROM User WHERE User_tg_name = ?', (user_tg_id,))
-        info_user = self.cursor.fetchone()
+        cursor.execute('SELECT User_id FROM User WHERE User_tg_name = ?', (user_tg_id,))
+        info_user = cursor.fetchone()
         print(info_user)
         if info_user is None:
-            self.cursor.execute('INSERT INTO User (User_tg_name) VALUES (?)', (user_tg_id,))
-            self.conn.commit()
-            self.cursor.execute('SELECT User_id FROM User WHERE User_tg_name = ?', (user_tg_id,))
-            info_user = self.cursor.fetchone()
+            cursor.execute('INSERT INTO User (User_tg_name) VALUES (?)', (user_tg_id,))
+            conn.commit()
+            cursor.execute('SELECT User_id FROM User WHERE User_tg_name = ?', (user_tg_id,))
+            info_user = cursor.fetchone()
         # Проверка наличия продукта
-        self.cursor.execute('SELECT Product_sku FROM Product WHERE Product_sku = ?', (int(product_data.sku),))
-        info_product = self.cursor.fetchone()
+        cursor.execute('SELECT Product_sku FROM Product WHERE Product_sku = ?', (int(product_data.sku),))
+        info_product = cursor.fetchone()
         if info_product is None:
-            self.cursor.execute('INSERT INTO Product (Product_sku) VALUES (?)', (int(product_data.sku),))
-            self.conn.commit()
-            self.cursor.execute('SELECT Product_sku FROM Product WHERE Product_sku = ?', (int(product_data.sku),))
-            info_product = self.cursor.fetchone()
+            cursor.execute('INSERT INTO Product (Product_sku) VALUES (?)', (int(product_data.sku),))
+            conn.commit()
+            cursor.execute('SELECT Product_sku FROM Product WHERE Product_sku = ?', (int(product_data.sku),))
+            info_product = cursor.fetchone()
         print(info_product)
         # Проверка наличия связи между пользователем и продуктом
-        info_bind = self.cursor.execute('SELECT * FROM User_product WHERE Product_in_id = ? AND User_in_id = ?',
+        info_bind = cursor.execute('SELECT * FROM User_product WHERE Product_in_id = ? AND User_in_id = ?',
                             (info_product[0], info_user[0]))
-        info_bind = self.cursor.fetchone()
+        info_bind = cursor.fetchone()
         print(info_user[0], info_product[0])
         if info_bind is None:
-            self.cursor.execute('INSERT INTO User_product (User_in_id, Product_in_id) VALUES (?, ?)',
+            cursor.execute('INSERT INTO User_product (User_in_id, Product_in_id) VALUES (?, ?)',
                                 (info_user[0], info_product[0]))
         print(info_bind)
-        self.conn.commit()
+        conn.commit()
+        conn.close()
+
 
     def insert_cost(self, product_data: parser.PriceInfo):
         """
@@ -126,20 +137,25 @@ class DataBaseConnect:
 
            :param product_data: Данные о продукте (объект PriceInfo).
         """
+        conn = sqlite3.connect('purchases.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        cursor = conn.cursor()
         try:
             # Проверка на None
             if product_data.sku is None or product_data.price is None or product_data.time is None:
                 print("Одно или несколько значений отсутствуют!")
                 return
             print(product_data.time)
-            self.cursor.execute('INSERT INTO Product_cost ( Product_sku, Date, Cost) VALUES (?, ?, ?)',
+            cursor.execute('INSERT INTO Product_cost ( Product_sku, Date, Cost) VALUES (?, ?, ?)',
                  (int(product_data.sku), product_data.time, product_data.price))
 
-            self.conn.commit()  # Коммит транзакции
+            conn.commit()  # Коммит транзакции
+            conn.close()
         except sqlite3.OperationalError:
             print("Такой таблицы не существует!")
             return
-        finally: self.conn.commit()
+        finally:
+            conn.commit()
+            conn.close()
 
     def delete_data(self):
         """
@@ -151,11 +167,17 @@ class DataBaseConnect:
         """
             Удаляет все таблицы из базы данных.
         """
-        self.cursor.execute('''DROP TABLE User''')
-        self.cursor.execute('''DROP TABLE User_product''')
-        self.cursor.execute('''DROP TABLE Product''')
-        self.cursor.execute('''DROP TABLE Product_cost''')
+        conn = sqlite3.connect('purchases.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        cursor = conn.cursor()
+
+        cursor.execute('''DROP TABLE User''')
+        cursor.execute('''DROP TABLE User_product''')
+        cursor.execute('''DROP TABLE Product''')
+        cursor.execute('''DROP TABLE Product_cost''')
         print("Tables Dropped!")
+
+        conn.commit()
+        conn.close()
 
     def get_cost_by_sku(self, sku):
         """
@@ -164,13 +186,17 @@ class DataBaseConnect:
             :param sku: Артикул продукта.
             :return: Данные о стоимости продукта.
         """
+        conn = sqlite3.connect('purchases.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        cursor = conn.cursor()
         try:
-            self.cursor.execute("SELECT * FROM Product_cost where Product_sku = ?", (int(sku),))
+            cursor.execute("SELECT * FROM Product_cost where Product_sku = ?", (int(sku),))
         except sqlite3.OperationalError:
             print("Такой таблицы не существует!")
             return
-        result = self.cursor.fetchall()
+        result = cursor.fetchall()
         print("Получил!:", result)
+        conn.commit()
+        conn.close()
         return result
 
 
